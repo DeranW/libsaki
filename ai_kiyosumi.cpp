7da9489fadcc9897b04e8faefeeaf9f27dd85f56
Paper <33085004+DeranW@users.noreply.github.com>
2017-10-26 21:12:06 +0800

    Create ai_kiyosumi.cpp

---
+++ ai_kiyosumi.cpp
@@ -0,0 +1,20 @@
+#include "ai_kiyosumi.h"
+#include "util.h"
+
+
+
+namespace saki
+{
+
+
+
+Action AiNodoka::think(const TableView &view, Limits &limits)
+{
+
+    return Ai::think(view, limits);
+}
+
+
+
+} // namespace saki
+
